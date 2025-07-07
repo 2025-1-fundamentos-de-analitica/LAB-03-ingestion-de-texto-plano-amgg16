5b1593c165e822c64f22c788c817ba4eebb74986
amgg16 <aguaring@unal.edu.co>
2025-07-07 11:14:09 -0500

    .

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -4,6 +4,7 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 
 # pylint: disable=import-outside-toplevel
 
+import pandas as pd
 
 def pregunta_01():
     """
@@ -18,3 +19,40 @@ def pregunta_01():
 
 
     """
+    ruta = "files/input/clusters_report.txt"
+    lineas = open(ruta, encoding="utf-8").read().splitlines()[4:]     #descartar encabezado
+    
+    registros = []
+    trozo = []
+
+    for linea in lineas + ['']: #'' para procesar última línea
+        if linea.strip():
+            trozo.append(linea.strip())
+        else:
+            if trozo:
+                bloque = " ".join(trozo)
+                partes = bloque.split()
+                
+                num_cluster = int(partes[0])
+                cantidad = int(partes[1])
+                porcentaje = float(partes[2].replace(',', '.'))
+                
+
+                texto_claves = " ".join(partes[4:])
+                texto_claves = " ".join(texto_claves.split())
+                if texto_claves.endswith('.'):
+                    texto_claves = texto_claves[:-1]
+                texto_claves = texto_claves.replace(' ,', ',')
+                
+                registros.append({
+                    'cluster': num_cluster,
+                    'cantidad_de_palabras_clave': cantidad,
+                    'porcentaje_de_palabras_clave': porcentaje,
+                    'principales_palabras_clave': texto_claves
+                })
+                trozo = []
+    
+    df_clusters = pd.DataFrame(registros)
+    return df_clusters
+
+print(pregunta_01())
